94d994cf10cdec5987122a92c4f033bb6e89c5bc
Azdren Ymeri <azdreny@gmail.com>
2019-08-29 18:32:49 +0200

    initial commit

---
+++ src/js/test/index.test.js
@@ -0,0 +1,8 @@
+
+
+describe('index.js', ()=> {
+  test('This is just a test', ()=> {
+    expect('hello').toBe('hello');
+  });
+  // you can use another describe to gather more tests together
+});
