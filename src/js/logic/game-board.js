2f4cbe0909ec652de92109f93b8af871ced79467
Azdren Ymeri <azdreny@gmail.com>
2019-08-30 14:53:44 +0200

    gameboard placing ships implemented

--- src/js/logic/game-board.js
+++ src/js/logic/game-board.js
@@ -2,10 +2,10 @@
 const GameBoard = () => {
   const missedHits = 0;
   const grid = new Array(100).fill().map((v, i)=>i);
+  const shipsOnBoard = [];
   const placeShip = (ship, coordinate, position) => {
-    // first  lets get the first position we gonna add
-    const shipCoordinates = new Array(ship.length);
-
+    // first  lets get the  positions we gonna add
+    const shipCoordinates = [];
     if (position === 'h') {
       ship.body.forEach((item) => {
         shipCoordinates.push(grid.indexOf(grid[coordinate]));
@@ -14,19 +14,37 @@ const GameBoard = () => {
     } else if (position === 'v') {
       ship.body.forEach((item) => {
         shipCoordinates.push(grid.indexOf(grid[coordinate]));
-        coordinate*=10;
+        coordinate+=10;
       });
     }
-    if (shipCoordinates.every((position) => position !== typeof 'string')) {
+    // checks if we are adding a ship on top of another
+    if (shipCoordinates
+        .every((position) => typeof grid[position] !== 'string')) {
+      // if it equials to -1 means we are outside the bounds of grid
+      if (shipCoordinates[shipCoordinates.length-1] === -1) {
+        return false;
+      }
       shipCoordinates.forEach((coord) => {
-        grid[coord] = 1;
+        grid[coord] = ship.name;
       });
+      shipsOnBoard.push(ship);
       return true;
     }
     return false;
   };
+
   const acceptHit = (coordinate) => {
+    if (grid[coordinate] === typeof 'string') {
+      const shipThatGotHit = findShip(grid[coordinate]);
+      console.log(shipThatGotHit.name);
+      return true;
+    }
+    return false;
   };
+  const findShip = (name) => {
+    return shipsOnBoard.fill((ship) => ship.name === name);
+  };
+
   return {placeShip, grid, missedHits, acceptHit};
 };
 
--- src/js/test/logic/game-board.test.js
+++ src/js/test/logic/game-board.test.js
@@ -2,23 +2,54 @@ import {Ship} from './../../logic/ship';
 import {GameBoard} from './../../logic/game-board';
 
 describe('game-board.js', ()=>{
-  let ship;
+  let submarine;
+  let carrier;
+  let battleShip;
   let board;
-  let isPlaced;
+
   beforeAll(() => {
-    ship = new Ship(4);
+    submarine = new Ship('submarine', 3);
+    carrier = new Ship('carrier', 5);
+    battleShip = new Ship('battleShip', 4);
     board = new GameBoard();
-    isPlaced = board.placeShip(ship, 12, 'h');
   });
 
-  test('ship should be placed on the board', ()=> {
-    expect(isPlaced).toBe(true);
+  test('Submarine should be placed VERTICALLY on the board', ()=> {
+    expect(board.placeShip(submarine, 12, 'h')).toBe(true);
   });
 
-  test('the ship is placed on correct place', () => {
-    expect(board.grid[12]).toBe(1);
-    expect(board.grid[13]).toBe(1);
-    expect(board.grid[14]).toBe(1);
-    expect(board.grid[15]).toBe(1);
+  test('Carrier should be placed HORIZONTALLY on the board', ()=> {
+    expect(board.placeShip(carrier, 54, 'v')).toBe(true);
+  });
+
+  test('You can\'t add battleship outside the grid', ()=> {
+    expect(board.placeShip(battleShip, 99, 'h')).toBe(false);
+  });
+  test('You can\'t add battleShip on occupied coordinate', ()=> {
+    expect(board.placeShip(battleShip, 54, 'h')).toBe(false);
+  });
+  test('battleShip should be placed horizontaly on the board', ()=> {
+    expect(board.placeShip(battleShip, 96, 'h')).toBe(true);
+  });
+
+  test('submarine is placed on correct coordinates', ()=> {
+    expect(board.grid[12]).toBe(submarine.name);
+    expect(board.grid[13]).toBe(submarine.name);
+    expect(board.grid[14]).toBe(submarine.name);
+  });
+
+  test('carrier is placed on correct coordinates', ()=> {
+    expect(board.grid[54]).toBe(carrier.name);
+    expect(board.grid[64]).toBe(carrier.name);
+    expect(board.grid[74]).toBe(carrier.name);
+    expect(board.grid[84]).toBe(carrier.name);
+    expect(board.grid[94]).toBe(carrier.name);
+  });
+
+  test('battleShip is placed on correct coordinates', ()=> {
+    expect(board.grid[96]).toBe(battleShip.name);
+    expect(board.grid[97]).toBe(battleShip.name);
+    expect(board.grid[98]).toBe(battleShip.name);
+    expect(board.grid[99]).toBe(battleShip.name);
   });
 });
